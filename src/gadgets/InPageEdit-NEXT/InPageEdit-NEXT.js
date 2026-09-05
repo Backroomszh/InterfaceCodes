13cb3b878188e10f039b6e911d9885cbdbf80ed2
Corn Pig <179776930+Corn-Pig@users.noreply.github.com>
2026-09-05 22:40:48 +0800

    再次修改

--- src/gadgets/InPageEdit-NEXT/InPageEdit-NEXT.js
+++ src/gadgets/InPageEdit-NEXT/InPageEdit-NEXT.js
@@ -1,6 +1,8 @@
-document.head.append(
-  Object.assign(document.createElement('script'), {
-    src: 'https://cdn.jsdelivr.net/npm/@inpageedit/core/dist/index.js',
-    type: 'module'
-  })
-);
+$(function () {
+  document.body.append(
+    Object.assign(document.createElement('script'), {
+      src: 'https://cdn.jsdelivr.net/npm/@inpageedit/core/dist/index.js',
+      type: 'module'
+    })
+  );
+});
