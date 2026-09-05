b69f288350e5e7990a020c7f248e369330282bbe
Corn Pig <179776930+Corn-Pig@users.noreply.github.com>
2026-09-05 22:52:44 +0800

    测试

--- src/gadgets/InPageEdit-NEXT/Gadget-InPageEdit-NEXT.js
+++ src/gadgets/InPageEdit-NEXT/Gadget-InPageEdit-NEXT.js
@@ -1,7 +1,6 @@
-// InPageEdit NEXT
-document.body.append(
+document.head.append(
   Object.assign(document.createElement('script'), {
     src: 'https://cdn.jsdelivr.net/npm/@inpageedit/core/dist/index.js',
-    type: 'module',
+    type: 'module'
   })
-)
+);
