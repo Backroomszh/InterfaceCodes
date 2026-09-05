341b42ccea96fa707bc4d75319a5359a525a9145
Corn Pig <179776930+Corn-Pig@users.noreply.github.com>
2026-09-05 22:47:39 +0800

    修改错误

---
+++ src/gadgets/InPageEdit-NEXT/Gadget-InPageEdit-NEXT.js
@@ -0,0 +1,7 @@
+// InPageEdit NEXT
+document.body.append(
+  Object.assign(document.createElement('script'), {
+    src: 'https://cdn.jsdelivr.net/npm/@inpageedit/core/dist/index.js',
+    type: 'module',
+  })
+)
--- src/gadgets/InPageEdit-NEXT/InPageEdit-NEXT.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-  document.body.append(
-    Object.assign(document.createElement('script'), {
-      src: 'https://cdn.jsdelivr.net/npm/@inpageedit/core/dist/index.js',
-      type: 'module'
-    })
-  );
-});
